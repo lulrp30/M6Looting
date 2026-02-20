34cb5b802e326371dfb02aaea8066708a68635cb
lulrp30 <Repicazo@s.sfusd.edu>
2026-02-20 10:08:01 -0800

    M6-L9

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -8,7 +8,8 @@ func _ready() -> void:
 	canvas_group.material.set_shader_parameter("Line_thickness", 3.0)
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
-	
+	get_viewport().physics_object_picking_sort = true
+	get_viewport().physics_object_picking_first_only = true
 func set_outline_thickness(new_thickness: float) -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
 	
@@ -48,4 +49,21 @@ func _spawn_random_item() -> void:
 	var random_angle := randf_range(0.0,2.0 * PI)
 	var random_direction := Vector2 (1.0, 0.0).rotated(random_angle)
 	var random_distance := randf_range(60.0, 120.0)
-	loot_item.position = random_direction * random_distance
+	var land_position = random_direction * random_distance
+
+	const FLIGHT_TIME := 0.4
+	const HALF_FLIGHT_TIME := FLIGHT_TIME / 2.0
+	
+	var tween := create_tween()
+	tween.set_parallel()
+	loot_item.scale = Vector2(0.25, 0.25)
+	tween.tween_property(loot_item, "scale", Vector2(1.0,1.0), HALF_FLIGHT_TIME)
+	tween.tween_property(loot_item, "position:x", land_position.x, FLIGHT_TIME)
+	
+	tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.set_ease(Tween.EASE_OUT)
+	var jump_height := randf_range(30.0, 80.0)
+	tween.tween_property(loot_item, "position:y", land_position.y - jump_height, HALF_FLIGHT_TIME)
+	tween.set_ease(Tween.EASE_IN)
+	tween.tween_property(loot_item, "position:y", land_position.y, HALF_FLIGHT_TIME)
